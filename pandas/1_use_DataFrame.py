24774856db472d9776b73aac147ea7ac9e6591c7
Ahire Hitesh <hbahire2014@gmail.com>
2020-12-20 17:15:40 +0530

    updated

--- pandas/1_use_DataFrame.py
+++ pandas/1_use_DataFrame.py
@@ -3,7 +3,8 @@ import pandas
 mydataset = {
     'students': ['Hitesh', 'PD', 'Suyesh', 'Shubham'],
     'marks': [76, 76, 89, 87],
-    'addr': ['Dabli', 'Tehare', 'Malegaon', "Dabli"]
+    'addr': ['Dabli', 'Tehare', 'Malegaon', "Dabli"],
+    'age': [22, 22, 28, 21]
 }
 
 myvar = pandas.DataFrame(mydataset)
