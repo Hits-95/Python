b3888ee8313c2451f05db7e21666379f9e8db6c4
Ahire Hitesh <hbahire2014@gmail.com>
2020-12-23 22:41:22 +0530

    update mongodb url

--- MongoDb/3collection.py
+++ MongoDb/3collection.py
@@ -2,7 +2,7 @@
 '''Important: In MongoDB, a collection is not created until it gets content!'''
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017")
+myclient = pymongo.MongoClient("mongodb+srv://Hitesh:online@cluster0.w9ui5.mongodb.net/py_test?retryWrites=true&w=majority")
 mydb = myclient['py_test']
 mycoln = mydb['customer']
 print(mydb.list_collection_names())
--- MongoDb/4insert.py
+++ MongoDb/4insert.py
@@ -2,9 +2,11 @@
 '''Important: In MongoDB, a collection is not created until it gets content!'''
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017")
+myclient = pymongo.MongoClient("mongodb+srv://Hitesh:online@cluster0.w9ui5.mongodb.net/<dbname>?retryWrites=true&w=majority")
+
 mydb = myclient['py_test']
 mycoln = mydb['customer']
 mydict = {"name":"Shruti ","address":"dule"}
+print(mycoln.count_documents({}))
 result = mycoln.insert_one(mydict)
 print(result)
